f042e0d90e9c9fa84838f4573e3346cf54f77fa2
Anandh Kumar <anandh86@gmail.com>
2022-08-19 17:03:31 +0800

    Initial update

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+
+	var handl templateHandler
+
+	handl.filename = "index.html"
+
+	http.HandleFunc("/", handl.ServeHTTP)
+	http.ListenAndServe(":8080", nil)
+}
